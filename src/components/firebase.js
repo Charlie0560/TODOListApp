e7ae39298b5edc6c2e110873e8108ebf8f88a78a
Charlie0560 <chaitanyalokhande87@gmail.com>
2021-12-25 22:28:48 +0530

    final commits

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,11 +5,12 @@ import "firebase/compat/firestore";
 const firebaseapp = firebase.initializeApp({
   apiKey: "AIzaSyAjxoaf_kEQgqb8RThXx__mfEgFG4gzk6E",
   authDomain: "todoapplication-5a24a.firebaseapp.com",
+  databaseURL: "https://todoapplication-5a24a-default-rtdb.firebaseio.com",
   projectId: "todoapplication-5a24a",
   storageBucket: "todoapplication-5a24a.appspot.com",
   messagingSenderId: "43994274018",
   appId: "1:43994274018:web:d366c366950666faf126bb",
-  measurementId: "G-3QL5RPZ207",
+  measurementId: "G-3QL5RPZ207"
 });
 
 const db = firebaseapp.firestore();
